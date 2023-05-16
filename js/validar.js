ea5a5cd306349721a2fc4c6911680e98f31a74d5
FlorenciaCReiter <reiter.florencia7@hotmail.com>
2023-05-16 20:35:18 -0300

    Con Javascript

--- js/validar.js
+++ js/validar.js
@@ -1,12 +1,31 @@
 const formContacto = document.getElementById("formulario");
-const inputNombre = document.getElementById("name");
+const inputNombre = document.getElementById("nombre");
 const inputEmail = document.getElementById("email");
 const inputComentarios = document.getElementById("message");
+
+inputNombre.addEventListener("input", function (event) {
+    if(nombre.value == "") {
+    nombre.setCustomValidity("¡No has escrito nada en el nombre!");
+    } else {
+        nombre.setCustomValidity("");
+    }
+});
+
+inputEmail.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+    email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
+    } else {
+    email.setCustomValidity("");
+    }
+});
+
 
 formContacto.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    let inputFields = [{
+    if (inputNombre.textContent ==""){
+        alert("Debe ingresar nombre");
+    } let inputFields = [{
         field: inputNombre,
         message: "El nombre es requerido"
     }, {
